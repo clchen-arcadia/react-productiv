5bfa6070e4676d2f34307f7a38e34b5cd659f313
Philip Dimaano <pdimaano7@gmail.com>
2022-11-17 17:34:37 -0800

    Started writing tests for app components

---
+++ src/Todo.test.js
@@ -0,0 +1,19 @@
+import { render, fireEvent } from "@testing-library/react";
+import Todo from "./Todo";
+
+it("renders without crashing", function () {
+  const { container } = render(
+    <Todo
+      todo={{ id: 1, title: "TEST", description: "Testing", priority: 1 }}
+    />
+  );
+});
+
+it("contains expected term", function () {
+  const result = render(
+    <Todo
+      todo={{ id: 1, title: "TEST", description: "Testing", priority: 1 }}
+    />
+  );
+  expect(result.queryByText("TEST")).toBeInTheDocument();
+});
